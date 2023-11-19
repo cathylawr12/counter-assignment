3c33fd571fe7d8eb65881dcd9af5e28ae899f320
cathylawr12 <67284372+cathylawr12@users.noreply.github.com>
2023-11-19 16:42:00 -0500

    Update Counter.test.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,36 +1,22 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import Counter from '../components/Counter';
+//
+//
 
 beforeEach(() => {
-  render(<Counter />);
+
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
-  const counterHeading = screen.getByText(/Counter/i);
-  expect(counterHeading).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
-  const initialCount = screen.getByTestId('count');
-  expect(initialCount).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
-  const incrementButton = screen.getByText('+');
-  const countElement = screen.getByTestId('count');
-
-  fireEvent.click(incrementButton);
-  expect(countElement).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
-  const decrementButton = screen.getByText('-');
-  const countElement = screen.getByTestId('count');
-
-  fireEvent.click(decrementButton);
-  expect(countElement).toHaveTextContent('-1');
 });
